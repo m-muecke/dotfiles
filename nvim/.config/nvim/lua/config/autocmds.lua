a497959da0f850dc752e30459655df3595c0a459
Maximilian Muecke <muecke.maximilian@gmail.com>
2023-03-27 16:07:05 +0200

    feat: added lsp configs and changed autocmds to lua api

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,4 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber")
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufNewfile", "BufRead" }, {
+  pattern = ".Renviron",
+  callback = function()
+    vim.opt_local.filetype = "sh"
+  end,
+})
